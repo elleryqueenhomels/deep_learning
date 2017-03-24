0acce78b40eafd7bf2485238fc9584015d5ac320
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-03-24 22:40:23 +0800

    Update util.py
    add a get_facial_data() function.

--- util.py
+++ util.py
@@ -2,6 +2,7 @@
 # https://www.udemy.com/data-science-supervised-machine-learning-in-python
 import numpy as np
 import pandas as pd
+from sklearn.utils import shuffle
 
 def get_data(limit=None):
     print("Reading in and transforming data...")
@@ -41,4 +42,32 @@ def get_donut():
 
     X = np.concatenate([ X_inner, X_outer ])
     Y = np.array([0]*half_N + [1]*half_N)
+    return X, Y
+
+def get_facial_data(balance_ones=False):
+    # images are 48x48 = 2304 size vectors
+    # N = 35887
+    Y = []
+    X = []
+    first = True
+    for line in open('../python_test/facial_expression_recognition/fer2013.csv'):
+        if first:
+            first = False # skip the first line which is a header.
+        else:
+            row = line.split(',')
+            Y.append(int(row[0]))
+            X.append([int(e) for e in row[1].split()])
+
+    X, Y = np.array(X) / 255.0, np.array(Y)
+
+    if balance_ones:
+        # balance the 1 class
+        X0, Y0 = X[Y!=1, :], Y[Y!=1]
+        X1 = X[Y==1, :]
+        X1 = np.repeat(X1, 9, axis=0)
+        X = np.vstack([X0, X1])
+        Y = np.concatenate((Y0, [1]*len(X1)))
+
+    X, Y = shuffle(X, Y)
+
     return X, Y
