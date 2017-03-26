20bd59d9f6380a13a061ebbf9579e64122193c34
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-03-27 01:43:26 +0800

    Update ann.py

--- ann.py
+++ ann.py
@@ -7,7 +7,7 @@ class ANN:
 		self.layers = layers
 		self.activation_type = activation_type
 
-	def fit(self, X, Y, layers=None, activation_type=None, learning_rate=10e-7, epochs=20000, regularization1=0, regularization2=0):
+	def fit(self, X, Y, layers=None, activation_type=None, learning_rate=10e-5, epochs=20000, regularization1=0, regularization2=0):
 		if layers != None:
 			self.layers = layers
 		assert(self.layers != None)
@@ -75,9 +75,11 @@ class ANN:
 
 	def score(self, X, Y):
 		P = self.predict(X)
-		return np.mean(Y == P)
+		return self.classification_rate(Y, P)
 
 	def classification_rate(self, Y, P):
+		Y = np.squeeze(Y)
+		P = np.squeeze(P)
 		return np.mean(Y == P)
 
 	def activation(self, a):
