46d6c9d9bfc5bd94e795b23b0161a4a952a70df1
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-04-03 15:08:45 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -45,11 +45,11 @@ def experiment2():
 	if TRAIN_MODE:
 		# For MNIST Dataset the best hyperparameters: layers=[300], activation_type=2,
 		# epochs=500, batch_size=500, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01
-		model = ANN(layers=[100], activation_type=2)
+		model = ANN(layers=[300], activation_type=2)
 		print('\nBegin to training model.')
 		t0 = datetime.now()
 		# for MNIST: lr=10e-4, for Facial: lr=10e-5, using ReLU as activation both.
-		model.fit(Xtrain, Ytrain, epochs=500, batch_size=500, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01)
+		model.fit(Xtrain, Ytrain, epochs=500, batch_size=500, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01, debug=True, debug_points=200, valid_set=[Xtest[:1000], Ytest[:1000]])
 		print('\nTraining time:', (datetime.now() - t0), 'Train size:', len(Ytrain))
 
 		with open(MODEL_PATH, 'wb') as f:
@@ -73,7 +73,7 @@ def experiment3():
 	# plt.show()
 
 	model = ANN([10, 10], activation_type=2) # 5 hidden units or more
-	model.fit(X, Y, epochs=10000, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01)
+	model.fit(X, Y, epochs=5000, batch_size=50, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01, debug=True, valid_set=[X[-50:], Y[-50:]])
 	print('\nIn XOR: final score = %.8f%%' % (model.score(X, Y) * 100), '\n')
 
 def experiment4():
@@ -86,7 +86,7 @@ def experiment4():
 
 	model = ANN([10, 10], activation_type=2) # 8 hidden units or more
 	# model.fit(X, Y, epochs=10000, batch_size=100, learning_rate=10e-5, decay=0.99, momentum=0.9, regularization2=0.01)
-	model.fit(X, Y, epochs=5000, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01)
+	model.fit(X, Y, epochs=5000, batch_size=100, learning_rate=10e-4, decay=0.99, momentum=0.9, regularization2=0.01, debug=True, valid_set=[X[-200:], Y[-200:]])
 	print('\nIn Donut: final score = %.8f%%' % (model.score(X, Y) * 100), '\n')
 
 
