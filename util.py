401c8dbb62862d8843d3083d132a3d6120f655d4
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-04-05 17:29:11 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,3 @@
-# https://deeplearningcourses.com/c/data-science-supervised-machine-learning-in-python
-# https://www.udemy.com/data-science-supervised-machine-learning-in-python
 import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
