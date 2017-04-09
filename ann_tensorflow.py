9ed413c878c003424ba4eeaa1474a7cc7d8055ec
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-04-09 16:11:49 +0800

    Update ann_tensorflow.py

--- ann_tensorflow.py
+++ ann_tensorflow.py
@@ -47,7 +47,7 @@ class ANN(object):
 		N, D = X.shape
 		K = Y.shape[1]
 
-		# for debug: pre-process training set and validation set
+		# for debug: pre-process validation set
 		if debug:
 			if valid_set != None:
 				if len(valid_set) < 2 or len(valid_set[0]) != len(valid_set[1]):
