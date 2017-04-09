09c90d0937a9f5c7a9ed8d1a063646f138365ef8
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-04-09 23:13:15 +0800

    Update test_cnn.py

--- cnn/test_cnn.py
+++ cnn/test_cnn.py
@@ -2,27 +2,18 @@ import numpy as np
 
 from scipy.io import loadmat
 from sklearn.utils import shuffle
+# from utils import shuffle # If no sklearn installed, using this shuffle() instead
+from utils import rearrange
 from datetime import datetime
 
 from cnn_theano import CNN
-
-
-def rearrange(X):
-	# input is (32, 32, 3, N) from matlab file
-	# output is (N, 3, 32, 32) for Theano using
-	W, H, C, N = X.shape
-	out = np.zeros((N, C, W, H), dtype=np.float32)
-	for i in range(N):
-		for j in range(C):
-			out[i, j, :, :] = X[:, :, j, i]
-	return out / np.float32(255)
 
 
 def main():
 	# step 1: load the data, transform as needed
 	train = loadmat('../../python_test/data_set/train_32x32.mat')
 	test = loadmat('../../python_test/data_set/test_32x32.mat')
-	print('\nSuccessfully extract data!')
+	print('\nSuccessfully extract data!\n')
 
 	# Need to scale! don't leave as 0..255
 	# Y is a N x 1 matrix with values 1..10 (MATLAB indexes by 1)
@@ -38,14 +29,18 @@ def main():
 	del test
 	Ytest = Ytest.astype(np.int32)
 
+	print('Xtrain.shape =', Xtrain.shape)
+	print('Xtest.shape =', Xtest.shape)
+
 	Xtrain, Ytrain = shuffle(Xtrain, Ytrain)
+	Xtest, Ytest = shuffle(Xtest, Ytest)
 
 	# model = CNN([(20, 5, 5), (50, 5, 5)], pool_layer_sizes=[(2, 2), (2, 2)], hidden_layer_sizes=[200, 300, 100])
 	model = CNN([(20, 5, 5), (50, 5, 5)], hidden_layer_sizes=[500, 100])
 
 	print('\nBegin to train model...\n')
 	t0 = datetime.now()
-	model.fit(Xtrain, Ytrain, epochs=50, batch_sz=500, learning_rate=10e-6, decay=0.99, momentum=0.9, reg_l2=0.01, debug=True, cal_train=True, debug_points=100, valid_set=[Xtest[:1000], Ytest[:1000]])
+	model.fit(Xtrain, Ytrain, epochs=5, batch_sz=500, learning_rate=10e-6, decay=0.99, momentum=0.9, reg_l2=0.01, debug=True, cal_train=False, debug_points=100, valid_set=[Xtest[:1000], Ytest[:1000]])
 	print('\nTraing time:', (datetime.now() - t0))
 
 	t0 = datetime.now()
