a28a30d2eecef681dbdb811de359360ac7ad85f3
elleryqueenhomels <elleryqueenhomels@users.noreply.github.com>
2017-04-09 16:12:17 +0800

    Update ann_theano.py

--- ann_theano.py
+++ ann_theano.py
@@ -55,7 +55,7 @@ class ANN(object):
 		X = X.astype(np.float32)
 		Y = Y.astype(np.int32)
 
-		# for debug: pre-process training set and validation set
+		# for debug: pre-process validation set
 		if debug:
 			if valid_set != None:
 				if len(valid_set) < 2 or len(valid_set[0]) != len(valid_set[1]):
