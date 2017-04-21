0dc846336443a5f26aa2a0c7e3b617f5753ab4aa
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-04-21 15:20:08 +0800

    K-Means vs. GMM

--- unsupervised_ml/kmeans_vs_gmm.py
+++ unsupervised_ml/kmeans_vs_gmm.py
@@ -2,6 +2,7 @@
 # Using Purity to measure the "accuracy"
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.manifold import TSNE
 from util import get_mnist
@@ -14,15 +15,16 @@ def main():
 	_, _, X, Y = get_mnist()
 
 	K = len(set(Y))
+	max_iter = 50
 	print('K =', K)
 
 	print('\nBefore dimensionality reduction:')
 	print('input data shape:', X.shape)
 
-	_, R_kmeans = plot_k_means(X, K, max_iter=30)
+	_, R_kmeans = plot_k_means(X, K, max_iter=max_iter)
 	print('K-Means purity:', purity(Y, R_kmeans))
 
-	R_gmm, _, _, _ = gmm(X, K, max_iter=30, smoothing=1, eps=1e-16)
+	R_gmm, _, _, _ = gmm(X, K, max_iter=max_iter, smoothing=1, eps=1e-16)
 	print('GMM purity:', purity(Y, R_gmm))
 
 	print('\nAfter dimensionality reduction with t-SNE:')
@@ -30,11 +32,15 @@ def main():
 	tsne = TSNE()
 	Z = tsne.fit_transform(X)
 	print('input data shape:', Z.shape)
-
-	_, R_kmeans_tsne = plot_k_means(Z, K, max_iter=30)
+	
+	plt.scatter(Z[:,0], Z[:,1], c=Y, s=100, alpha=0.5)
+	plt.title('Original Distribution with True Labels before Clustering')
+	plt.show()
+	
+	_, R_kmeans_tsne = plot_k_means(Z, K, max_iter=max_iter)
 	print('K-Means purity:', purity(Y, R_kmeans_tsne))
 
-	R_gmm_tsne, _, _, _ = gmm(Z, K, max_iter=30, eps=0)
+	R_gmm_tsne, _, _, _ = gmm(Z, K, max_iter=max_iter, eps=0)
 	print('GMM purity:', purity(Y, R_gmm_tsne))
 
 
