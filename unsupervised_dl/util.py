ba22afcd0d8c4b162abc0bd3b0d5512eb2e14f59
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-04-23 23:27:39 +0800

    Utility

---
+++ unsupervised_dl/util.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+DATA_PATH = '../../python_test/data_set/MNIST_train.csv'
+
+def classification_rate(T, P):
+	return np.mean(T == P)
+
+
+def get_mnist(Ntrain=-1000):
+	# MNIST data:
+	# column 0 is labels
+	# column 1-785 is data, with values 0..255
+	# total size of csv: (42000, 1, 28, 28)
+	train = pd.read_csv(DATA_PATH).as_matrix().astype(np.float32)
+	np.random.shuffle(train)
+
+	Xtrain = train[:Ntrain, 1:] / np.float32(255)
+	Ytrain = train[:Ntrain, 0].astype(np.int32)
+
+	Xtest = train[Ntrain:, 1:] / np.float32(255)
+	Ytest = train[Ntrain:, 0].astype(np.int32)
+
+	return Xtrain, Ytrain, Xtest, Ytest
+
