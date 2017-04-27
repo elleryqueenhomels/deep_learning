69a8c217d0477cd6c5dd24d3396258eab0e32ff5
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-04-27 13:04:10 +0800

    Demo

--- dnn/_demo.py
+++ dnn/_demo.py
@@ -38,11 +38,27 @@ def test_no_pretrain(Xtrain, Ytrain, Xtest, Ytest):
 	print('Test accuracy=%.6f%%, Test size: %s' % (dnn.score(Xtest, Ytest)*100, len(Ytest)))
 
 
+def test_cnn(Xtrain, Ytrain, Xtest, Ytest):
+	N, D = Xtrain.shape
+	d = int(np.sqrt(D))
+	Xtrain = Xtrain.reshape(N, 1, d, d)
+	Xtest = Xtest.reshape(len(Xtest), 1, d, d)
+
+	cnn = CNN([(20, 5, 5), (50, 5, 5)], hidden_layer_sizes=[1000, 750, 500], UnsupervisedModel=RBM, activation_type=1, cost_type=1)
+
+	cnn.fit(Xtrain, Ytrain, Xtest, Ytest, epochs=3, batch_sz=100, debug=True, show_fig=False)
+
+	print('\nFinal Score:')
+	print('Train accuracy=%.6f%%, Train size: %s' % (dnn.score(Xtrain, Ytrain)*100, len(Ytrain)))
+	print('Test accuracy=%.6f%%, Test size: %s' % (dnn.score(Xtest, Ytest)*100, len(Ytest)))
+
+
 def main():
 	Xtrain, Ytrain, Xtest, Ytest = get_mnist(21000)
 	test_autoencoder(Xtrain, Ytrain, Xtest, Ytest)
 	test_rbm(Xtrain, Ytrain, Xtest, Ytest)
 	test_no_pretrain(Xtrain, Ytrain, Xtest, Ytest)
+	test_cnn(Xtrain, Ytrain, Xtest, Ytest)
 
 
 if __name__ == '__main__':
