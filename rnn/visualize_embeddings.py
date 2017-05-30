89ce6b10579540863a6604ffc95bd574c84b599c
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-05-31 01:36:31 +0800

    Visualize Word Embeddings learned by rnn_wiki.py

---
+++ rnn/visualize_embeddings.py
@@ -0,0 +1,31 @@
+# Using Dimensionality Reduction technique to visualize
+# Word Embeddings learned by RNN over Wikipedia dataset
+
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+
+
+def main(we_file, w2i_file, Model=PCA):
+	We = np.load(we_file)
+	V, D = We.shape
+
+	with open(w2i_file) as f:
+		word2idx = json.load(f)
+	idx2word = {v:k for k, v in word2idx.items()}
+
+	model = Model()
+	Z = model.fit_transform(We)
+	plt.scatter(Z[:,0], Z[:,1])
+	for i in range(V):
+		plt.annotate(s=idx2word[i], xy=(Z[i,0], Z[i,1]))
+	plt.show()
+
+
+if __name__ == '__main__':
+	we_file  = 'gru_nonorm_part1_word_embeddings.npy'
+	w2i_file = 'gru_nonorm_part1_wikipedia_word2idx.json'
+	main(we_file=we_file, w2i_file=w2i_file, Model=TSNE)
+
