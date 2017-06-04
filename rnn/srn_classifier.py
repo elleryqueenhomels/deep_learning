d12bb0d27619779db99bef846bd12d2ccfb1d5e2
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-06-04 23:56:51 +0800

    Using a Simple RNN to classify different poetry

--- rnn/srn_classifier.py
+++ rnn/srn_classifier.py
@@ -1,4 +1,4 @@
-# Using a Simple RNN to classify different poetry styles
+# Using Simple RNN to classify different poetry styles
 # Simple Recurrent Unit / Elman Unit
 
 import numpy as np
@@ -7,16 +7,17 @@ import theano.tensor as T
 import matplotlib.pyplot as plt
 
 from sklearn.utils import shuffle
-from util import init_weight, get_activation, get_poetry_classifier_data
+from util import init_weight, get_poetry_classifier_data
 
 
 class SimpleRNN(object):
-	def __init__(self, M, V, activation_type=1):
+	def __init__(self, M, V, activation=T.nnet.relu):
 		self.M = M # hidden layer size
 		self.V = V # vocabulary size
-		self.activation = get_activation(activation_type)
+		self.f = activation
 
-	def fit(self, X, Y, epochs=500, learning_rate=1e-4, decay = 0.9999, momentum=0.99, debug=False, Xvalid=None, Yvalid=None):
+	def fit(self, X, Y, epochs=500, learning_rate=1e-4, decay=0.9999, momentum=0.99, debug=False, Xvalid=None, Yvalid=None):
+		# for convenience
 		mu = momentum
 
 		# X: (N, T(n)), Y: (N, )
@@ -39,18 +40,21 @@ class SimpleRNN(object):
 
 		cost = -T.mean(T.log(py_x[thY]))
 		grads = T.grad(cost, self.params)
-		dparams = [theano.shared(np.zeros(p.get_value().shape)) for p in self.params]
+		dparams = [theano.shared(p.get_value() * 0) for p in self.params]
 		lr = T.scalar('learning_rate')
 
 		# momentum only
-		updates = []
-		for p, dp, g in zip(self.params, dparams, grads):
-			updates += [
-				(p, p + mu*dp - lr*g),
-				(dp, mu*dp - lr*g)
-			]
+		updates = [
+			(p, p + mu*dp - lr*g) for p, dp, g in zip(self.params, dparams, grads)
+		] + [
+			(dp, mu*dp - lr*g) for dp, g in zip(dparams, grads)
+		]
 
-		cost_predict_op = theano.function(inputs=[thX, thY], outputs=[cost, prediction], allow_input_downcast=True)
+		cost_predict_op = theano.function(
+			inputs=[thX, thY],
+			outputs=[cost, prediction],
+			allow_input_downcast=True
+		)
 
 		train_op = theano.function(
 			inputs=[thX, thY, lr],
@@ -71,15 +75,14 @@ class SimpleRNN(object):
 		for i in range(epochs):
 			X, Y = shuffle(X, Y)
 			if debug:
-				n_correct = 0
 				cost = 0
+				n_correct = 0
 
 			for j in range(N):
 				if debug:
 					c, p = train_debug_op(X[j], Y[j], learning_rate)
 					cost += c
-					if p == Y[j]:
-						n_correct += 1
+					n_correct += (p == Y[j])
 				else:
 					train_op(X[j], Y[j], learning_rate)
 				learning_rate *= decay
@@ -88,27 +91,24 @@ class SimpleRNN(object):
 				n_correct_valid = 0
 				for j in range(Nvalid):
 					p = self.predict_op(Xvalid[j])
-					if p == Yvalid[j]:
-						n_correct_valid += 1
+					n_correct_valid += (p == Yvalid[j])
 				costs.append(cost)
-				score = n_correct / N
-				score_valid = n_correct_valid / Nvalid
-				print('epoch=%d, cost=%.6f, score=%.6f%%' % (i, cost, score*100))
-				print('validation score=%.6f%%' % (score_valid*100))
+				score = float(n_correct) / N
+				score_valid = float(n_correct_valid) / Nvalid
+				print('epoch: %d, cost: %.6f, score: %.6f%%' % (i, cost, score*100))
+				print('validation score: %.6f%%' % (score_valid*100))
 
 		if debug:
 			cost, n_correct, n_correct_valid = 0, 0, 0
 			for j in range(N):
 				c, p = cost_predict_op(X[j], Y[j])
 				cost += c
-				if p == Y[j]:
-					n_correct += 1
+				n_correct += (p == Y[j])
 			for j in range(Nvalid):
 				p = self.predict_op(Xvalid[j])
-				if p == Yvalid[j]:
-					n_correct_valid += 1
-			score = n_correct / N
-			score_valid = n_correct_valid / Nvalid
+				n_correct_valid += (p == Yvalid[j])
+			score = float(n_correct) / N
+			score_valid = float(n_correct_valid) / Nvalid
 			print('Finally: cost=%.6f, score=%.6f%%' % (cost, score*100))
 			print('Finally: validation score=%.6f%%' % (score_valid*100))
 
@@ -131,7 +131,7 @@ class SimpleRNN(object):
 		def recurrence(x_t, h_t1):
 			# returns h(t), y(t)
 			# x_t: scalar, h_t: (M, ), y_t: (1, K)
-			h_t = self.activation(self.Wx[x_t] + h_t1.dot(self.Wh) + self.bh)
+			h_t = self.f(self.Wx[x_t] + h_t1.dot(self.Wh) + self.bh)
 			y_t = T.nnet.softmax(h_t.dot(self.Wo) + self.bo)
 			return h_t, y_t
 
@@ -158,7 +158,7 @@ class SimpleRNN(object):
 		np.savez(filename, *array)
 
 	@staticmethod
-	def load(filename, activation_type=1):
+	def load(filename, activation=T.nnet.relu):
 		npz = np.load(filename)
 		Wx = npz['arr_0']
 		Wh = npz['arr_1']
@@ -167,21 +167,21 @@ class SimpleRNN(object):
 		Wo = npz['arr_4']
 		bo = npz['arr_5']
 		V, M = Wx.shape
-		rnn = SimpleRNN(M, V, activation_type)
+		rnn = SimpleRNN(M, V, activation)
 		rnn.set(Wx, Wh, bh, h0, Wo, bo)
 		return rnn
 
 
 def train_poetry():
-	X, Y, V = get_poetry_classifier_data(load_cached=False)
-	print('type(X):', type(X), 'type(Y):', type(Y))
-	print('\nV=%d, len(X)=%d\n' % (V, len(X)))
+	X, Y, V = get_poetry_classifier_data(load_cached=True)
+	print('\ntype(X):', type(X), 'type(Y):', type(Y))
+	print('\nV = %d, len(X) = %d\n' % (V, len(X)))
 	X, Y = shuffle(X, Y)
 	Nvalid = 100
 	Xvalid, Yvalid = X[-Nvalid:], Y[-Nvalid:]
 	X, Y = X[:-Nvalid], Y[:-Nvalid]
-	rnn = SimpleRNN(30, V, activation_type=1)
-	rnn.fit(X, Y, epochs=1000, learning_rate=1e-4, decay=0.999999, debug=True, Xvalid=Xvalid, Yvalid=Yvalid)
+	rnn = SimpleRNN(30, V, activation=T.nnet.relu)
+	rnn.fit(X, Y, epochs=500, learning_rate=1e-4, decay=0.99999, debug=True, Xvalid=Xvalid, Yvalid=Yvalid)
 
 
 if __name__ == '__main__':
