d3041ea8bc338ac8b76ea17e78914c41f96dabe5
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-07-27 22:50:53 +0800

    Use Policy Gradient to solve CartPole

--- rl_deep/cartpole/policy_gradient_theano.py
+++ rl_deep/cartpole/policy_gradient_theano.py
@@ -99,7 +99,7 @@ class PolicyModel:
 		X = np.atleast_2d(X)
 		actions = np.atleast_1d(actions)
 		advantages = np.atleast_1d(advantages)
-		self.train_op(X, actions advantages)
+		self.train_op(X, actions, advantages)
 
 	def predict(self, X):
 		X = np.atleast_2d(X)
@@ -171,4 +171,134 @@ class ValueModel:
 	def predict(self, X):
 		X = np.atleast_2d(X)
 		return self.predict_op(X)
+
+
+def play_one_td(env, pmodel, vmodel, gamma, max_iters=3000):
+	observation = env.reset()
+	done = False
+	totalreward = 0
+	iters = 0
+
+	while not done and iters < max_iters:
+		action = pmodel.sample_action(observation)
+		prev_observation = observation
+		observation, reward, done, info = env.step(action)
+
+		# if done:
+		# 	reward = -200
+
+		# update the model
+		V_next = vmodel.predict(observation)
+		G = reward + gamma * np.max(V_next) # np.max flatten V_next from (1, ) to ()
+		advantage = G - vmodel.predict(prev_observation)
+		pmodel.partial_fit(prev_observation, action, advantage)
+		vmodel.partial_fit(prev_observation, G)
+
+		if reward == 1: # if we changed the reward to -200 while done
+			totalreward += reward
+		iters += 1
+
+	return totalreward
+
+
+def play_one_mc(env, pmodel, vmodel, gamma, max_iters=3000):
+	observation = env.reset()
+	done = False
+	totalreward = 0
+	iters = 0
+
+	states = []
+	actions = []
+	rewards = []
+
+	reward = 0
+	while not done and iters < max_iters:
+		action = pmodel.sample_action(observation)
+
+		states.append(observation)
+		actions.append(action)
+		rewards.append(reward)
+
+		prev_observation = observation
+		observation, reward, done, info = env.step(action)
+
+		# if done:
+		# 	reward = -200
+
+		if reward == 1: # if we changed the reward to -200 while done
+			totalreward += reward
+		iters += 1
+
+	# save the final (s, a, r) tuple
+	action = pmodel.sample_action(observation)
+	states.append(observation)
+	actions.append(action)
+	rewards.append(reward)
+
+	returns = []
+	advantages = []
+	G = 0
+	for s, r in zip(reversed(states), reversed(rewards)):
+		returns.append(G)
+		advantages.append(G - vmodel.predict(s)[0])
+		G = r + gamma * G
+	returns.reverse()
+	advantages.reverse()
+
+	# update the model
+	pmodel.partial_fit(states, actions, advantages)
+	vmodel.partial_fit(states, returns)
+
+	return totalreward
+
+
+def main():
+	# gym.envs.register(
+	# 	id='MyCartPole-v0',
+	# 	entry_point='gym.envs.classic_control:CartPoleEnv',
+	# 	max_episode_steps=10000,
+	# 	reward_threshold=9975.0,
+	# )
+	# env = gym.make('MyCartPole-v0')
+	env = gym.make('CartPole-v0')
+
+	# if 'monitor' in sys.argv:
+	# 	filenema = os.path.basename(__file__).split('.')[0]
+	# 	monitor_dir = './' + filenema + '_' + str(datetime.now())
+	# 	env = wrappers.Monitor(env, monitor_dir)
+
+	D = env.observation_space.shape[0]
+	K = env.action_space.n
+
+	pmodel = PolicyModel(D, K, hidden_layer_sizes=[10], activation=T.tanh, learning_rate=1e-3, momentum=0.8, decay=0.999)
+	vmodel = ValueModel(D, hidden_layer_sizes=[30], activation=T.tanh, learning_rate=1e-4)
+	gamma = 0.99
+
+	N = 500
+	totalrewards = np.empty(N)
+	for n in range(N):
+		totalreward = play_one_mc(env, pmodel, vmodel, gamma)
+		# totalreward = play_one_td(env, pmodel, vmodel, gamma)
+		totalrewards[n] = totalreward
+		if n % 20 == 0:
+			print('episode: %d, current reward: %s, avg reward (last 100 episodes): %s' % (n, totalreward, totalrewards[max(0, n-99):(n+1)].mean()))
+
+	print('avg reward for last 100 episodes: %s' % totalrewards[-100:].mean())
+	print('total steps: %s' % totalrewards.sum())
+
+	plt.plot(totalrewards)
+	plt.title('Rewards')
+	plt.show()
+
+	plot_running_avg(totalrewards)
+
+	if 'monitor' in sys.argv:
+		filenema = os.path.basename(__file__).split('.')[0]
+		monitor_dir = './' + filenema + '_' + str(datetime.now())
+		env = wrappers.Monitor(env, monitor_dir)
+		play_one_mc(env, pmodel, vmodel, gamma)
+
+
+if __name__ == '__main__':
+	main()
 
